c197aaffd44d676dba2a8e2a3932e3a4fdb39d06
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-04-13 21:18:10 +0530

    OTP verification added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,12 +4,9 @@ export const createUser = async (req, res) => {
     try {
 
         // HW : req.body theke requered data for user and then create a user
-        const { name } = req.body
 
 
-        const user = await User.create({
-            name: name,
-        });
+        const user = await User.create(req.body);
         // HW : create function in utils folder which will create a 6 digit random otp and will return it  ex-op : 124872
         // save the otp in user data
 
